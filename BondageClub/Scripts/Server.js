206578592242d6d1284e50ef61f94a34f5cee6fc
Ellie <62729616+elliesec@users.noreply.github.com>
2020-07-04 15:44:02 +0100

    Login message now informs the user when server connection is lost

--- BondageClub/Scripts/Server.js
+++ BondageClub/Scripts/Server.js
@@ -1,13 +1,18 @@
 "use strict";
 var ServerSocket = null;
 var ServerURL = "http://localhost:4288";
-var ServerRelog = null;
 var ServerBeep = {};
 var ServerBeepAudio = new Audio();
+var ServerIsConnected = false;
+var ServerReconnectCount = 0;
+var ServerDidDisconnect = false;
 
 // Loads the server events
 function ServerInit() {
 	ServerSocket = io(ServerURL);
+	ServerSocket.on("connect", ServerConnect);
+	ServerSocket.on("reconnecting", ServerReconnecting);
+	ServerSocket.on("event", function (data) {console.log(data);});
 	ServerSocket.on("ServerMessage", function (data) { console.log(data); });
 	ServerSocket.on("ServerInfo", function (data) { ServerInfo(data); });
 	ServerSocket.on("CreationResponse", function (data) { CreationResponse(data); });
@@ -35,6 +40,25 @@ function ServerInit() {
 	ServerBeepAudio.src = "Audio/BeepAlarm.mp3";
 }
 
+function ServerSetConnected(connected) {
+	ServerIsConnected = connected;
+	ServerDidDisconnect = !connected;
+	if (connected) {
+		ServerReconnectCount = 0;
+	}
+	LoginUpdateMessage();
+}
+
+function ServerConnect() {
+	console.info("Server connection established");
+	ServerSetConnected(true);
+}
+
+function ServerReconnecting() {
+	ServerReconnectCount++;
+	LoginUpdateMessage();
+}
+
 // When the server sends some information to the client, we keep it in variables
 function ServerInfo(data) {
 	if (data.OnlinePlayers != null) CurrentOnlinePlayers = data.OnlinePlayers;
@@ -43,6 +67,12 @@ function ServerInfo(data) {
 
 // When the server disconnects, we enter in "Reconnect Mode"
 function ServerDisconnect(data) {
+	ServerSetConnected(false);
+	console.warn("Server connection lost");
+	if (data) {
+		console.warn(data);
+	}
+
 	if (Player.Name != "") {
 		if (CurrentScreen != "Relog") {
 
@@ -75,7 +105,6 @@ function ServerDisconnect(data) {
 
 		}
 	}
-	else if (CurrentScreen == "Login") LoginMessage = TextGet((data != null) ? data : "ErrorDisconnectedFromServer");
 }
 
 // Sends a message to the server
