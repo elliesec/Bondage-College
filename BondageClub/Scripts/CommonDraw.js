ecab16b07471628b97ce189371f12e4b0cfa9b0a
Ellie <62729616+elliesec@users.noreply.github.com>
2020-09-06 13:25:11 +0100

    Better handling for default colours

--- BondageClub/Scripts/CommonDraw.js
+++ BondageClub/Scripts/CommonDraw.js
@@ -156,12 +156,15 @@ function CommonDrawAppearanceBuild(C, {
 		}
 
 		// Check if we need to copy the color of another asset
-		var InheritColor = Layer.InheritColor || (Color == "Default" ? (CA.Asset.InheritColor || CA.Asset.Group.InheritColor) : null);
+		var InheritColor = Layer.InheritColor || (Color == "Default" ? (A.InheritColor || AG.InheritColor) : null);
 		if (InheritColor != null) {
 			var ParentAsset = InventoryGet(C, InheritColor);
 			if (ParentAsset != null) Color = ParentAsset.Color;
 		}
 
+		if (Color === "Default" && A.DefaultColor) {
+			Color = Array.isArray(A.DefaultColor) ? A.DefaultColor[Layer.ColorIndex] : A.DefaultColor;
+		}
 
 		// Draw the item on the canvas (default or empty means no special color, # means apply a color, regular text means we apply that text)
 		if ((Color != null) && (Color.indexOf("#") == 0) && Layer.AllowColorize) {
