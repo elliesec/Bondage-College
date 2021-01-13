3fdb906fddb7eaa6d86a770e4540cdae2530f294
Ellie <62729616+elliesec@users.noreply.github.com>
2021-01-13 01:34:21 +0000

    Pose system fixes

--- BondageClub/Scripts/Character.js
+++ BondageClub/Scripts/Character.js
@@ -110,6 +110,7 @@ function CharacterReset(CharacterID, CharacterAssetFamily) {
 		IsNpc: function () { return (this.AccountName.substring(0, 4) === "NPC_" || this.AccountName.substring(0, 4) === "NPC-"); },
 		GetDifficulty: function () { return ((this.Difficulty == null) || (this.Difficulty.Level == null) || (typeof this.Difficulty.Level !== "number") || (this.Difficulty.Level < 0) || (this.Difficulty.Level > 3)) ? 1 : this.Difficulty.Level; },
 		IsInverted: function () { return this.Pose.indexOf("Suspension") >= 0; },
+		CanChangeToPose: function(Pose) { return CharacterCanChangeToPose(this, Pose); }
 	};
 
 	// If the character doesn't exist, we create it
@@ -458,6 +459,21 @@ function CharacterAddPose(C, NewPose) {
 }
 
 /**
+ * Checks whether the given character can change to the named pose unaided
+ * @param {Character} C - The character to check
+ * @param {string} poseName - The name of the pose to check for
+ * @returns {boolean} - Returns true if the character has no conflicting items and is not prevented from changing to
+ * the provided pose
+ */
+function CharacterCanChangeToPose(C, poseName) {
+	const pose = PoseFemale3DCG.find(P => P.Name === poseName);
+	if (!pose) return false;
+	const poseCategory = pose.Category;
+	if (!CharacterItemsHavePoseAvailable(C, poseCategory, pose)) return false;
+	return !C.Appearance.some(item => InventoryGetItemProperty(item, "FreezeActivePose").includes(poseCategory));
+}
+
+/**
  * Checks if a certain pose is whitelisted and available for the pose menu
  * @param {Character} C - Character to check for the pose
  * @param {string} Type - Pose type to check for within items
@@ -465,7 +481,7 @@ function CharacterAddPose(C, NewPose) {
  * @returns {boolean} - TRUE if the character has the pose available
  */
 function CharacterItemsHavePoseAvailable(C, Type, Pose) {
-	var PossiblePoses = PoseFemale3DCG.filter(P => P.Category == Type || P.Category == "BodyFull").map(P => P.Name);
+	const ConflictingPoses = PoseFemale3DCG.filter(P => P.Name !== Pose && (P.Category == Type || P.Category == "BodyFull")).map(P => P.Name);
 
 	for (let i = 0, Item = null; i < C.Appearance.length; i++) {
 		Item = C.Appearance[i];
@@ -474,10 +490,11 @@ function CharacterItemsHavePoseAvailable(C, Type, Pose) {
 		if (WhitelistActivePose != null && WhitelistActivePose.includes(Pose)) continue;
 
 		const AllowActivePose = InventoryGetItemProperty(Item, "AllowActivePose");
-		if (AllowActivePose != null && AllowActivePose.find(P => PossiblePoses.includes(P) && C.AllowedActivePose.includes(P))) return false;
+		if (AllowActivePose != null && AllowActivePose.find(P => C.AllowedActivePose.includes(P))) continue;
+		// if (AllowActivePose != null && AllowActivePose.find(P => ConflictingPoses.includes(P) && C.AllowedActivePose.includes(P))) return false;
 
 		const SetPose = InventoryGetItemProperty(Item, "SetPose", true);
-		if (SetPose != null && SetPose.find(P => PossiblePoses.includes(P))) return false;
+		if (SetPose != null && SetPose.find(P => ConflictingPoses.includes(P))) return false;
 	}
 	return true;
 }
@@ -490,17 +507,20 @@ function CharacterItemsHavePoseAvailable(C, Type, Pose) {
  */
 function CharacterItemsHavePose(C, Pose) { 
 	if (C.ActivePose != null && C.AllowedActivePose.includes(Pose) && (typeof C.ActivePose == "string" && C.ActivePose == Pose || Array.isArray(C.ActivePose) && C.ActivePose.includes(Pose))) return true;
-	for (let A = 0; A < C.Appearance.length; A++) {
-		if ((C.Appearance[A].Property != null) && (C.Appearance[A].Property.SetPose != null) && (C.Appearance[A].Property.SetPose.includes(Pose)))
-			return true;
-		else
-			if (C.Appearance[A].Asset.SetPose != null && (C.Appearance[A].Asset.SetPose.includes(Pose)))
-				return true;
-			else
-				if (C.Appearance[A].Asset.Group.SetPose != null && (C.Appearance[A].Asset.Group.SetPose.includes(Pose)))
-					return true;
-	}
-	return false;
+	return CharacterDoItemsSetPose(C, Pose);
+}
+
+/**
+ * Checks whether the items on a character set a given pose on the character
+ * @param {Character} C - The character to check
+ * @param {string} pose - The name of the pose to check for
+ * @returns {boolean} - Returns true if the character is wearing an item that sets the given pose, false otherwise
+ */
+function CharacterDoItemsSetPose(C, pose) {
+	return C.Appearance.some(item => {
+		const setPose = InventoryGetItemProperty(item, "SetPose", true);
+		return setPose && setPose.includes(pose);
+	});
 }
 
 /**
@@ -537,11 +557,12 @@ function CharacterLoadPose(C) {
 	C.Pose = [];
 	C.AllowedActivePose = [];
 	
-	for (let A = 0; A < C.Appearance.length; A++) {
+	for (let i = 0, Item = null; i < C.Appearance.length; i++) {
+		Item = C.Appearance[i];
 		const AllowActivePose = InventoryGetItemProperty(Item, "AllowActivePose");
 		if (Array.isArray(AllowActivePose)) AllowActivePose.forEach(Pose => C.AllowedActivePose.push(Pose));
 
-		const SetPose = InventoryGetItemProperty(C.Appearance[A], "SetPose", true);
+		const SetPose = InventoryGetItemProperty(Item, "SetPose", true);
 		if (SetPose != null) CharacterAddPose(C, SetPose);
 	}
 	
@@ -553,10 +574,10 @@ function CharacterLoadPose(C) {
 		var ActivePoses = C.ActivePose
 			.map(CP => PoseFemale3DCG.find(P => P.Name == CP))
 			.filter(P => P);
-		
+
 		for (let P = 0; P < ActivePoses.length; P++) {
 			var HasPose = C.Pose.includes(ActivePoses[P].Name);
-			var IsAllowed = C.AllowedActivePose.includes(ActivePoses[P].Name) && CharacterItemsHavePoseAvailable(Player, ActivePoses[P].Category, ActivePoses[P].Name);
+			var IsAllowed = C.AllowedActivePose.includes(ActivePoses[P].Name) && CharacterItemsHavePoseAvailable(C, ActivePoses[P].Category, ActivePoses[P].Name);
 			var MissingGroup = !Poses.find(Pose => Pose.Category == "BodyFull") && !Poses.find(Pose => Pose.Category == ActivePoses[P].Category);
 			var IsFullBody = C.Pose.length > 0 && ActivePoses[P].Category == "BodyFull";
 			if (!HasPose && (IsAllowed || (MissingGroup && !IsFullBody)))
@@ -1145,12 +1166,12 @@ function CharacterAppearsInverted(C) {
 
 /**
  * Checks whether the given character can kneel unaided
- * @param C
- * @return {boolean}
+ * @param {Character} C - The character to check
+ * @returns {boolean} - Returns true if the character is capable of kneeling unaided, false otherwise
  */
 function CharacterCanKneel(C) {
 	if (C.Effect.includes("Freeze") || C.Effect.includes("ForceKneel")) return false;
 	if (C.Pose == null) return true;
 	if (C.Pose.includes("Suspension") || C.Pose.includes("Hogtied")) return false;
-	return CharacterItemsHavePoseAvailable(C, "BodyLower", "Kneel");
+	return C.CanChangeToPose("Kneel");
 }
