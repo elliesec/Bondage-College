d0bef6a917d451c58fb6c375d14093de7234cc42
Ellie <62729616+elliesec@users.noreply.github.com>
2021-01-23 13:19:55 +0000

    Final halo updates

--- BondageClub/Screens/Inventory/HairAccessory3/Halo/Halo.js
+++ BondageClub/Screens/Inventory/HairAccessory3/Halo/Halo.js
@@ -1,5 +1,16 @@
 const InventoryHairAccessory3HaloBrightnessInputId = "InventoryHairAccessory3HaloBrightness";
 
+const InventoryHairAccessory3HaloOptions = [
+	{
+		Name: "Default",
+		Property: { Type: null },
+	},
+	{
+		Name: "Broken",
+		Property: { Type: "Broken" },
+	},
+];
+
 /**
  * Loads the extended item's properties
  * @returns {void} - Nothing
@@ -7,14 +18,22 @@ const InventoryHairAccessory3HaloBrightnessInputId = "InventoryHairAccessory3Hal
 function InventoryHairAccessory3HaloLoad() {
 	const C = CharacterAppearanceSelection;
 	const item = DialogFocusItem;
-	const Property = item.Property = item.Property || {};
-	if (typeof Property.Opacity !== "number") {
-		Property.Opacity = 0;
-		CharacterRefresh(C, false);
+	const property = item.Property = item.Property || {};
+	let refresh = false;
+	if (typeof property.Opacity !== "number") {
+		property.Opacity = 0;
+		refresh = true;
 	}
 
+	if (!InventoryHairAccessory3HaloOptions.find(option => option.Property.Type === property.Type)) {
+		property.Type = InventoryHairAccessory3HaloOptions[0].Property.Type;
+		refresh = true;
+	}
+
+	if (refresh) CharacterRefresh(C, false);
+
 	const brightnessInput = ElementCreateRangeInput(
-		InventoryHairAccessory3HaloBrightnessInputId, Property.Opacity, item.Asset.MinOpacity, item.Asset.MaxOpacity, 0.01, "lightbulb");
+		InventoryHairAccessory3HaloBrightnessInputId, property.Opacity, item.Asset.MinOpacity, item.Asset.MaxOpacity, 0.01, "lightbulb");
 	if (brightnessInput) {
 		brightnessInput.addEventListener(
 			"input",
@@ -28,17 +47,32 @@ function InventoryHairAccessory3HaloLoad() {
  * @returns {void} - Nothing
  */
 function InventoryHairAccessory3HaloDraw() {
+	const asset = DialogFocusItem.Asset;
+	const property = DialogFocusItem.Property;
+
 	// Draw the header and item
-	DrawRect(1387, 125, 225, 275, "white");
+	DrawRect(1387, 55, 225, 275, "white");
 	DrawImageResize(
-		"Assets/" + DialogFocusItem.Asset.Group.Family + "/" + DialogFocusItem.Asset.DynamicGroupName + "/Preview/" + DialogFocusItem.Asset.Name +
-		".png", 1389, 127, 221, 221);
-	DrawTextFit(DialogFocusItem.Asset.Description, 1500, 375, 221, "black");
+		"Assets/" + asset.Group.Family + "/" + asset.DynamicGroupName + "/Preview/" + asset.Name +
+		".png", 1389, 57, 221, 221);
+	DrawTextFit(asset.Description, 1500, 310, 221, "black");
 
 	MainCanvas.textAlign = "right";
-	DrawTextFit("Brightness", 1475, 500, 400, "#fff", "#000");
-	ElementPosition(InventoryHairAccessory3HaloBrightnessInputId, 1725, 500, 400);
+	DrawTextFit("Brightness", 1475, 430, 400, "#fff", "#000");
+	ElementPosition(InventoryHairAccessory3HaloBrightnessInputId, 1725, 430, 400);
 	MainCanvas.textAlign = "center";
+
+	DrawTextFit(DialogFind(Player, "InventoryHairAccessory3HaloType"), 1500, 530, 800, "#fff", "#000");
+
+	InventoryHairAccessory3HaloOptions.forEach((option, i) => {
+		const x = ExtendedXY[InventoryHairAccessory3HaloOptions.length][i][0];
+		const y = ExtendedXY[InventoryHairAccessory3HaloOptions.length][i][1] + 80;
+		const isSelected = property.Type === option.Property.Type;
+
+		DrawButton(x, y, 225, 275, "", isSelected ? "#888" : "#fff", null, null, isSelected);
+		DrawImage("Screens/Inventory/" + asset.Group.Name + "/" + asset.Name + "/" + option.Name + ".png", x + 2, y);
+		DrawTextFit(DialogFind(Player, "InventoryHairAccessory3HaloType" + option.Name), x + 112, y + 250, 225, "#000");
+	});
 }
 
 /**
@@ -50,6 +84,17 @@ function InventoryHairAccessory3HaloClick() {
 	if (MouseIn(1885, 25, 90, 90)) {
 		return InventoryHairAccessory3HaloExit();
 	}
+
+	const C = CharacterGetCurrent() || CharacterAppearanceSelection;
+
+	InventoryHairAccessory3HaloOptions.forEach((option, i) => {
+		const x = ExtendedXY[InventoryHairAccessory3HaloOptions.length][i][0];
+		const y = ExtendedXY[InventoryHairAccessory3HaloOptions.length][i][1] + 80;
+
+		if (MouseIn(x, y, 225, 275)) {
+			ExtendedItemHandleOptionClick(C, InventoryHairAccessory3HaloOptions, option, false, true);
+		}
+	});
 }
 
 /**
