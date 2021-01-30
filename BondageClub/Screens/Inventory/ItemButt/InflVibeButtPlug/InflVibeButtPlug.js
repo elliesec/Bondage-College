232786dc4ec038dc9dbcf122a21777bb78904159
Ellie <62729616+elliesec@users.noreply.github.com>
2021-01-30 22:55:40 +0000

    Final preview image refactors

--- BondageClub/Screens/Inventory/ItemButt/InflVibeButtPlug/InflVibeButtPlug.js
+++ BondageClub/Screens/Inventory/ItemButt/InflVibeButtPlug/InflVibeButtPlug.js
@@ -28,40 +28,40 @@ function InventoryItemButtInflVibeButtPlugDraw() {
 }
 
 // Catches the item extension clicks
-function InventoryItemButtInflVibeButtPlugClick() {
+function InventoryItemButtInflVibeButtPlugClick(actionPrefix) {
+	actionPrefix = actionPrefix || "InflVibeButtPlug"
 	if (MouseIn(1885, 25, 90, 90)) DialogFocusItem = null;
-	else if (MouseIn(1200, 775, 200, 55) && (DialogFocusItem.Property.InflateLevel !== 0)) InventoryItemButtInflVibeButtPlugInflation(0 - DialogFocusItem.Property.InflateLevel);
-	else if (MouseIn(1550, 775, 200, 55) && (DialogFocusItem.Property.InflateLevel !== 1)) InventoryItemButtInflVibeButtPlugInflation(1 - DialogFocusItem.Property.InflateLevel);
-	else if (MouseIn(1200, 835, 200, 55) && (DialogFocusItem.Property.InflateLevel !== 2)) InventoryItemButtInflVibeButtPlugInflation(2 - DialogFocusItem.Property.InflateLevel);
-	else if (MouseIn(1550, 835, 200, 55) && (DialogFocusItem.Property.InflateLevel !== 3)) InventoryItemButtInflVibeButtPlugInflation(3 - DialogFocusItem.Property.InflateLevel);
-	else if (MouseIn(1375, 895, 200, 55) && (DialogFocusItem.Property.InflateLevel !== 4)) InventoryItemButtInflVibeButtPlugInflation(4 - DialogFocusItem.Property.InflateLevel);
-	else if (MouseIn(1200, 550, 200, 55) && (DialogFocusItem.Property.Intensity !== -1)) InventoryItemButtInflVibeButtPlugSetIntensity(-1 - DialogFocusItem.Property.Intensity);
-	else if (MouseIn(1550, 550, 200, 55) && (DialogFocusItem.Property.Intensity !== 0)) InventoryItemButtInflVibeButtPlugSetIntensity(0 - DialogFocusItem.Property.Intensity);
-	else if (MouseIn(1200, 610, 200, 55) && (DialogFocusItem.Property.Intensity !== 1)) InventoryItemButtInflVibeButtPlugSetIntensity(1 - DialogFocusItem.Property.Intensity);
-	else if (MouseIn(1550, 610, 200, 55) && (DialogFocusItem.Property.Intensity !== 2)) InventoryItemButtInflVibeButtPlugSetIntensity(2 - DialogFocusItem.Property.Intensity);
-	else if (MouseIn(1375, 670, 200, 55) && (DialogFocusItem.Property.Intensity !== 3)) InventoryItemButtInflVibeButtPlugSetIntensity(3 - DialogFocusItem.Property.Intensity);
+	else if (MouseIn(1200, 775, 200, 55) && (DialogFocusItem.Property.InflateLevel !== 0)) InventoryItemButtInflVibeButtPlugInflation(0 - DialogFocusItem.Property.InflateLevel, actionPrefix);
+	else if (MouseIn(1550, 775, 200, 55) && (DialogFocusItem.Property.InflateLevel !== 1)) InventoryItemButtInflVibeButtPlugInflation(1 - DialogFocusItem.Property.InflateLevel, actionPrefix);
+	else if (MouseIn(1200, 835, 200, 55) && (DialogFocusItem.Property.InflateLevel !== 2)) InventoryItemButtInflVibeButtPlugInflation(2 - DialogFocusItem.Property.InflateLevel, actionPrefix);
+	else if (MouseIn(1550, 835, 200, 55) && (DialogFocusItem.Property.InflateLevel !== 3)) InventoryItemButtInflVibeButtPlugInflation(3 - DialogFocusItem.Property.InflateLevel, actionPrefix);
+	else if (MouseIn(1375, 895, 200, 55) && (DialogFocusItem.Property.InflateLevel !== 4)) InventoryItemButtInflVibeButtPlugInflation(4 - DialogFocusItem.Property.InflateLevel, actionPrefix);
+	else if (MouseIn(1200, 550, 200, 55) && (DialogFocusItem.Property.Intensity !== -1)) InventoryItemButtInflVibeButtPlugSetIntensity(-1 - DialogFocusItem.Property.Intensity, actionPrefix);
+	else if (MouseIn(1550, 550, 200, 55) && (DialogFocusItem.Property.Intensity !== 0)) InventoryItemButtInflVibeButtPlugSetIntensity(0 - DialogFocusItem.Property.Intensity, actionPrefix);
+	else if (MouseIn(1200, 610, 200, 55) && (DialogFocusItem.Property.Intensity !== 1)) InventoryItemButtInflVibeButtPlugSetIntensity(1 - DialogFocusItem.Property.Intensity, actionPrefix);
+	else if (MouseIn(1550, 610, 200, 55) && (DialogFocusItem.Property.Intensity !== 2)) InventoryItemButtInflVibeButtPlugSetIntensity(2 - DialogFocusItem.Property.Intensity, actionPrefix);
+	else if (MouseIn(1375, 670, 200, 55) && (DialogFocusItem.Property.Intensity !== 3)) InventoryItemButtInflVibeButtPlugSetIntensity(3 - DialogFocusItem.Property.Intensity, actionPrefix);
 }
 
 // Sets the inflatable vibe butt plug pump Level
-function InventoryItemButtInflVibeButtPlugInflation(Modifier) {
+function InventoryItemButtInflVibeButtPlugInflation(Modifier, actionPrefix) {
 	var C = (Player.FocusGroup != null) ? Player : CurrentCharacter;
 	DialogFocusItem.Property.InflateLevel = DialogFocusItem.Property.InflateLevel + Modifier;
 	if (C.ID == 0) ServerPlayerAppearanceSync();
 	var Dictionary = [];
 	Dictionary.push({Tag: "DestinationCharacter", Text: C.Name, MemberNumber: C.MemberNumber});
 	Dictionary.push({Tag: "SourceCharacter", Text: Player.Name, MemberNumber: Player.MemberNumber});
-    ChatRoomPublishCustomAction("InflVibeButtPlug_Pump" + ((Modifier > 0) ? "pumps" : "deflates") + "To" + DialogFocusItem.Property.InflateLevel, true, Dictionary);
+    ChatRoomPublishCustomAction(actionPrefix + "_Pump" + ((Modifier > 0) ? "pumps" : "deflates") + "To" + DialogFocusItem.Property.InflateLevel, true, Dictionary);
 
 }
 
 // Sets the inflatable vibe butt plug vibration Level
-function InventoryItemButtInflVibeButtPlugSetIntensity(Modifier) {
+function InventoryItemButtInflVibeButtPlugSetIntensity(Modifier, actionPrefix) {
 	var C = (Player.FocusGroup != null) ? Player : CurrentCharacter;
 	DialogFocusItem.Property.Intensity = DialogFocusItem.Property.Intensity + Modifier;
 	if (DialogFocusItem.Property.Intensity == -1) DialogFocusItem.Property.Effect = ["Egged"];
 	if (DialogFocusItem.Property.Intensity >= 0) DialogFocusItem.Property.Effect = ["Egged", "Vibrating"];
 	CharacterLoadEffect(C);
 	if (C.ID == 0) ServerPlayerAppearanceSync();
-
-	ChatRoomPublishCustomAction( "InflVibeButtPlug_Vibe" + ((Modifier > 0) ? "Increase" : "Decrease") + "To" + DialogFocusItem.Property.Intensity, true, [{Tag: "DestinationCharacterName", Text: C.Name, MemberNumber: C.MemberNumber}]);
+	ChatRoomPublishCustomAction( actionPrefix + "_Vibe" + ((Modifier > 0) ? "Increase" : "Decrease") + "To" + DialogFocusItem.Property.Intensity, true, [{Tag: "DestinationCharacterName", Text: C.Name, MemberNumber: C.MemberNumber}]);
 }
