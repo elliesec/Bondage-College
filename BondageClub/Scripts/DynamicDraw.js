62fb3c5b122da2add62bfce18969347ba8e10e02
Ellie <62729616+elliesec@users.noreply.github.com>
2021-01-31 22:42:13 +0000

    Refactors wooden sign dynamic text handling into a reusable function

---
+++ BondageClub/Scripts/DynamicDraw.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const DynamicDrawTextRegex = /^(?:\w|[ ~!$#%*+])*$/;
+const DynamicDrawTextInputPattern = "(?:\\w|[ ~!$#%*+])*";
+
+const DynamicDrawTextDefaultOptions = {
+	font: CommonGetFont(30),
+	textAlign: "center",
+	textBaseline: "middle",
+	color: "#000",
+	effect: undefined,
+	width: undefined,
+};
+
+const DynamicDrawTextEffect = {
+	BURN: "burn",
+};
+
+const DynamicDrawTextEffects = {
+	[DynamicDrawTextEffect.BURN]: {
+		before(text, ctx, x, y, { width }) {
+			ctx.fillStyle = "#000";
+			ctx.fillText(text, x - 1, y - 1, width);
+		},
+	},
+};
+
+function DynamicDrawText(text, ctx, x, y, options) {
+	options = options || {};
+	const { font, textAlign, textBaseline, color, effect, width } = options = Object.assign({}, DynamicDrawTextDefaultOptions, options);
+	const effectDef = DynamicDrawTextEffects[effect] || {};
+
+	ctx.font = font;
+	ctx.textAlign = textAlign;
+	ctx.textBaseline = textBaseline;
+
+	if (typeof effectDef.before === "function") effectDef.before(text, ctx, x, y, options);
+
+	ctx.fillStyle = color;
+	ctx.fillText(text, x, y, width);
+
+	if (typeof effectDef.after === "function") effectDef.after(text, ctx, x, y, options);
+}
