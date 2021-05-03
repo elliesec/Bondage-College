fdfeb64acf7e9eb2b324ad59871200dabbefa953
Ellie <62729616+elliesec@users.noreply.github.com>
2021-05-03 17:07:32 +0100

    Lint fixes

--- BondageClub/Screens/Character/Book/Book.js
+++ BondageClub/Screens/Character/Book/Book.js
@@ -8,76 +8,76 @@ let BookMarkdown = null;
 let BookDefaultMarkedRenderer = null;
 
 const BookRenderer = {
-    heading(text, level, raw, slugger) {
-        const slug = slugger.slug(text).trim().replaceAll(/(^-+|-+$)/g, "");
-        return `
+	heading(text, level, raw, slugger) {
+		const slug = slugger.slug(text).trim().replaceAll(/(^-+|-+$)/g, "");
+		return `
             <h${level}>
               <a id="${slug}" class="anchor" href="#${slug}"></a>
               ${text}
             </h${level}>`;
-    },
-    link(href, title, text) {
-        const externalLink = typeof href === "string" && !href.startsWith("#");
-        let linkText = BookDefaultMarkedRenderer.link(href, title, text);
-        if (externalLink) {
-            linkText = linkText.replace(/^<a/, "<a target=\"_blank\"");
-        }
-        return linkText;
-    },
+	},
+	link(href, title, text) {
+		const externalLink = typeof href === "string" && !href.startsWith("#");
+		let linkText = BookDefaultMarkedRenderer.link(href, title, text);
+		if (externalLink) {
+			linkText = linkText.replace(/^<a/, "<a target=\"_blank\"");
+		}
+		return linkText;
+	},
 	image(href, title, text) {
-    	if (/^\.\.?\//.test(href)) {
-    		href = `Books/Markdown/${BookCurrent.name}/${href}`;
-	    }
-    	return `<img src="${href}" title=${title} alt=${text}/>`;
-	}
+		if (/^\.\.?\//.test(href)) {
+			href = `Books/Markdown/${BookCurrent.name}/${href}`;
+		}
+		return `<img src="${href}" title=${title} alt=${text}/>`;
+	},
 };
 
 function BookLoad() {
-    location.hash = "";
-    const bookBox = ElementCreateDiv("BookBox");
-    if (BookCurrent) {
-        Promise.all([BookLoadMarked(), BookLoadCurrent()]).then(() => {
-            marked.use({renderer: BookRenderer});
-            bookBox.innerHTML = marked(BookMarkdown);
-        });
-    }
+	location.hash = "";
+	const bookBox = ElementCreateDiv("BookBox");
+	if (BookCurrent) {
+		Promise.all([BookLoadMarked(), BookLoadCurrent()]).then(() => {
+			marked.use({ renderer: BookRenderer });
+			bookBox.innerHTML = marked(BookMarkdown);
+		});
+	}
 }
 
 function BookRun() {
-    ElementPositionFix("BookBox", 36, 500, 40, 1000, 890);
-    document.getElementById("BookBox").style.fontSize = null;
-    DrawButton(1885, 25, 90, 90, "", "#fff", "Icons/Exit.png", TextGet("Exit"));
+	ElementPositionFix("BookBox", 36, 500, 40, 1000, 890);
+	document.getElementById("BookBox").style.fontSize = null;
+	DrawButton(1885, 25, 90, 90, "", "#fff", "Icons/Exit.png", TextGet("Exit"));
 }
 
 function BookClick() {
-    // Exit Button
-    if (MouseIn(1885, 25, 90, 90)) BookExit();
+	// Exit Button
+	if (MouseIn(1885, 25, 90, 90)) BookExit();
 }
 
 function BookExit() {
-    ElementRemove("BookBox");
-    if (typeof BookReturn === "function") {
-        BookReturn();
-    }
-    BookCurrent = null;
-    BookReturn = null;
-    location.hash = "";
+	ElementRemove("BookBox");
+	if (typeof BookReturn === "function") {
+		BookReturn();
+	}
+	BookCurrent = null;
+	BookReturn = null;
+	location.hash = "";
 }
 
 function BookLoadMarked() {
-    if (typeof window.marked === "function") {
-        return Promise.resolve();
-    } else {
-        return CommonLoadScript("https://cdnjs.cloudflare.com/ajax/libs/marked/1.2.7/marked.min.js")
-            .then(() => (BookDefaultMarkedRenderer = new marked.Renderer()));
-    }
+	if (typeof window.marked === "function") {
+		return Promise.resolve();
+	} else {
+		return CommonLoadScript("https://cdnjs.cloudflare.com/ajax/libs/marked/1.2.7/marked.min.js")
+			.then(() => (BookDefaultMarkedRenderer = new marked.Renderer()));
+	}
 }
 
 function BookLoadCurrent() {
-    return new Promise((resolve) => {
-        CommonGet(`Books/Markdown/${BookCurrent.name}/${BookCurrent.name}.md`, (xhr) => {
-            BookMarkdown = xhr.responseText;
-            resolve(BookMarkdown);
-        });
-    });
+	return new Promise((resolve) => {
+		CommonGet(`Books/Markdown/${BookCurrent.name}/${BookCurrent.name}.md`, (xhr) => {
+			BookMarkdown = xhr.responseText;
+			resolve(BookMarkdown);
+		});
+	});
 }
--- BondageClub/Scripts/Book.js
+++ BondageClub/Scripts/Book.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const Books = [];
 
 let BookNames;
@@ -5,38 +7,38 @@ let BookAuthors;
 let BookTags;
 
 class Book {
-    constructor(bookDefinition) {
-        this.name = bookDefinition.name;
-        this.title = "";
-        this.description = "";
-        this.authorKey = bookDefinition.author;
-        this.author = "";
-        this.tags = Array.isArray(bookDefinition.tags) ? bookDefinition.tags : [];
-    }
+	constructor(bookDefinition) {
+		this.name = bookDefinition.name;
+		this.title = "";
+		this.description = "";
+		this.authorKey = bookDefinition.author;
+		this.author = "";
+		this.tags = Array.isArray(bookDefinition.tags) ? bookDefinition.tags : [];
+	}
 }
 
 function BookLoadAll() {
-    BookDefinitions.forEach((bookDef) => Books.push(new Book(bookDef)));
+	BookDefinitions.forEach((bookDef) => Books.push(new Book(bookDef)));
 
-    // Initialise the text caches for book names, authors and tags
-    BookNames = new TextCache("Books/Books.csv");
-    BookAuthors = new TextCache("Books/Authors.csv");
-    BookTags = new TextCache("Books/Tags.csv");
+	// Initialise the text caches for book names, authors and tags
+	BookNames = new TextCache("Books/Books.csv");
+	BookAuthors = new TextCache("Books/Authors.csv");
+	BookTags = new TextCache("Books/Tags.csv");
 
-    // Register cache rebuild listeners so that books are translated on language changes
-    BookNames.onRebuild(BookLoadNames);
-    BookAuthors.onRebuild(BookLoadAuthors);
+	// Register cache rebuild listeners so that books are translated on language changes
+	BookNames.onRebuild(BookLoadNames);
+	BookAuthors.onRebuild(BookLoadAuthors);
 }
 
 function BookLoadNames(bookNames) {
-    Books.forEach((book) => {
-        book.title = bookNames.get(`${book.name}`);
-        book.description = bookNames.get(`${book.name}_Description`);
-    });
+	Books.forEach((book) => {
+		book.title = bookNames.get(`${book.name}`);
+		book.description = bookNames.get(`${book.name}_Description`);
+	});
 }
 
 function BookLoadAuthors(bookAuthors) {
-    Books.forEach((book) => {
-        book.author = bookAuthors.get(book.authorKey);
-    });
+	Books.forEach((book) => {
+		book.author = bookAuthors.get(book.authorKey);
+	});
 }
